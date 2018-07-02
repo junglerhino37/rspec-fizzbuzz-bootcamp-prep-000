c9d52cc47808568e2d1b5e52e32b0797e5ba02b5
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 20:03:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(n)
-if (n%3 
+  if (n%3==0 && n%5==0)
+  puts "Fizzbuzz"
+elsif(n%3==0)
+    puts "Fizz"
+  elsif(n%5==0)
+    puts "Buzz"
+  end
 end
